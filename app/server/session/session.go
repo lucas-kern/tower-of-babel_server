040de06ab3d8a1acdcf79cc836b544905a2b2aac
lucas-kern <lucasdkern@gmail.com>
2023-01-04 20:51:48 -0600

    Messing with sessions

--- app/server/session/session.go
+++ app/server/session/session.go
@@ -4,31 +4,34 @@ import (
 	"log"
 	"net/http"
 
-	"fmt"
+	"github.com/lucas-kern/tower-of-babel_server/app/server/database"
+	"github.com/lucas-kern/tower-of-babel_server/app/model"
 
-	"github.com/Don-V/RSB-backend/app/backend/database"
-	"github.com/Don-V/RSB-backend/app/backend/operations/transaction"
-	"github.com/Don-V/RSB-backend/app/model"
-	"github.com/kidstuff/mongostore"
+	"github.com/lucas-kern/mongostore"
 	"github.com/gorilla/sessions"
 )
 
+// Session is used to create and manage sessions 
+// it uses a store to store the user sessions once they are created
+
+// TODO: test that this all works Then create users and test that when a user is created they are given only one session at a time
+
 // Store represents a datastore for the session
-// It is supposed to be a wrapper for gorilla's [sessions.Store]
+// It is a wrapper for gorilla's [sessions.Store] interface
 type Store interface {
 	Get(r *http.Request, name string) (Session, error)
 	New(r *http.Request, name string) (Session, error)
 	Save(r *http.Request, w http.ResponseWriter, s Session) error
 }
 
-// internalStore is the store that is used by the server
+// sessionStore is the store that is used by the server
 // It uses the [internalSession]
 // It uses [mongostore.MongoStore] for [MaxAge]
-type internalStore struct {
+type sessionStore struct {
 	store sessions.Store
 }
 
-func (st internalStore) Get(r *http.Request, name string) (Session, error) {
+func (st sessionStore) Get(r *http.Request, name string) (Session, error) {
 	sess, err := st.store.Get(r, name)
 	sess.Options.HttpOnly = true
 	return &internalSession{
@@ -36,41 +39,40 @@ func (st internalStore) Get(r *http.Request, name string) (Session, error) {
 	}, err
 }
 
-func (st internalStore) New(r *http.Request, name string) (Session, error) {
+func (st sessionStore) New(r *http.Request, name string) (Session, error) {
 	sess, err := st.store.New(r, name)
 	return &internalSession{
 		session: sess,
 	}, err
 }
 
-func (st internalStore) Save(r *http.Request, w http.ResponseWriter, s Session) error {
+func (st sessionStore) Save(r *http.Request, w http.ResponseWriter, s Session) error {
 	return st.store.Save(r, w, s.(internalSession).session)
 }
 
-func (st internalStore) MaxAge(age int) {
+func (st sessionStore) MaxAge(age int) {
 	if mst, ok := st.store.(*mongostore.MongoStore); ok {
 		mst.MaxAge(age)
 	}
 }
 
 // Session represents a session for a request
-// It is supposed to be a wrapper for gorilla's [sessions.Session]
+// It is a wrapper for gorilla's [sessions.Session]
 type Session interface {
 	Name() string
 	Save(r *http.Request, w http.ResponseWriter) error
 	Store() Store
 	Set(key string, value interface{})
 	Get(key string) (interface{}, bool)
-	Assign(result transaction.Result) error
+	Values()  map[interface{}]interface{}
 	IsAuth() bool
 	IsNew() bool
 	ID() string
 	Clear()
 	Destroy()
-	All() map[interface{}]interface{}
 }
 
-// internalStore is the session used by the server
+// sessionStore is the session used by the server
 // It wraps gorilla's [sessions.Session]
 type internalSession struct {
 	session *sessions.Session
@@ -85,7 +87,7 @@ func (s internalSession) Save(r *http.Request, w http.ResponseWriter) error {
 }
 
 func (s internalSession) Store() Store {
-	return internalStore{
+	return sessionStore{
 		store: s.session.Store(),
 	}
 }
@@ -110,20 +112,20 @@ func (s internalSession) Clear() {
 
 // AuthSetup setups a session to have the needed user data from an authentication transaction
 // As of right now, this is an operation
-func (s internalSession) Assign(result transaction.Result) error {
-	data, ok := result.GetData().(map[string]interface{})
-	if !ok {
-		err := fmt.Errorf("From Session: Invalid transaction type in SessionAuthSetup")
-		log.Println(err)
-		return err
-	}
+// func (s internalSession) Assign(result transaction.Result) error {
+// 	data, ok := result.GetData().(map[string]interface{})
+// 	if !ok {
+// 		err := fmt.Errorf("From Session: Invalid transaction type in SessionAuthSetup")
+// 		log.Println(err)
+// 		return err
+// 	}
 
-	s.Set("userid", data["userid"].(string)) //Need for casting? // should be used for user-session cross refernce
-	s.Set("email", data["email"].(string))
-	s.Set("username", data["username"].(string))
+// 	s.Set("userid", data["userid"].(string)) //Need for casting? // should be used for user-session cross refernce
+// 	s.Set("email", data["email"].(string))
+// 	s.Set("username", data["username"].(string))
 
-	return nil
-}
+// 	return nil
+// }
 
 // IsAuth returns true if this is a valid session that has been setup
 func (s internalSession) IsAuth() bool {
@@ -142,20 +144,20 @@ func (s internalSession) ID() string {
 }
 
 //TODO: remove
-func (s internalSession) All() map[interface{}]interface{} {
+func (s internalSession) Values() map[interface{}]interface{} {
 	return s.session.Values
 }
 
 // NewSessionStore returns a new session store for managing sessions
 // TODO: move this to a more specific file/package
 func NewSessionStore(keyPairs ...[]byte) Store {
-	return internalStore{
+	return sessionStore{
 		store: sessions.NewCookieStore(keyPairs...),
 	}
 }
 
 // NewMongoSessionStore returns a new session store for managing sessions
-// The store is implemted in a MongoDB database
+// The store is implemented in a MongoDB database
 // TODO: move this to a more specific file/package
 func NewMongoSessionStore(c model.Collection, keyPairs ...[]byte) Store {
 	expire := 60 * 60 //seconds
@@ -163,7 +165,8 @@ func NewMongoSessionStore(c model.Collection, keyPairs ...[]byte) Store {
 	if !ok {
 		log.Fatalf("Collection passed in is not MongoCollection{}. It was %T", c) //TODO: return err, panic -> recover? vs Fatalf
 	}
-	store := &internalStore{
+	store := &sessionStore{
+		//MAJOR ISSUE find out why it won't update to my new mongostore project
 		store: mongostore.NewMongoStore(coll.Collection, expire, true, keyPairs...),
 	}
 	store.MaxAge(expire)
